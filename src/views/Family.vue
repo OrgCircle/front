12e5fae44ea347249299d54b21708adfb02375ca
Maxime Valette <valettemaxime26@gmail.com>
2020-12-14 10:57:41 +0100

    add view my-family

---
+++ src/views/Family.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="family">
+    <h1>
+      {{title}}
+    </h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+  data: function () {
+    return {
+        title: 'Ma famille'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
