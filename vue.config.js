60da294ab3cca1cba1dd8cbe2925ba5e45d4d8f6
FabienTreve <trevefabien@gmail.com>
2020-12-17 12:22:33 +0100

    fix url

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const PORT = process.env.PORT || 8081;
+const URL = process.env.URL || "http://localhost:8081/api";
 
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
-    proxy: "http://localhost:" + PORT + "/api",
+    proxy: URL,
   }
 }
